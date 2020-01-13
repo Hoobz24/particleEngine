d84b9d79fcfc6aa804fd28aa49ca34adf007313a
Andrew Tucker <45807017+Hoobz24@users.noreply.github.com>
2020-01-12 20:51:23 -0800

    sand, water, walls, conway particles

--- particleSimulation/src/sample/Main.java
+++ particleSimulation/src/sample/Main.java
@@ -1,9 +1,15 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -13,6 +19,7 @@ import javafx.scene.paint.*;
 import javafx.scene.canvas.*;
 import javafx.scene.input.MouseEvent;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 
@@ -21,6 +28,11 @@ public class Main extends Application {
     private AnimationTimer timer;
     final double SCENE_WIDTH = 800;
     final double SCENE_HEIGHT = 800;
+    private boolean spawning = false;
+    private double mx;
+    private double my;
+    private ArrayList<String> partTypes = new ArrayList<String>();
+    private int sel = 1;
     ArrayList<particle> parts = new ArrayList<particle>();
 
 
@@ -34,11 +46,15 @@ public class Main extends Application {
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
         canvas.setOnMouseClicked(mouseHandler);
-
-        parts.add(new particle(10, 10, "SAND", parts.size()));
-        parts.add(new particle(10, 20, "SAND", parts.size()));
+        canvas.setOnMousePressed(mousePressed);
+        canvas.setOnMouseReleased(mouseReleased);
+        canvas.setOnMouseDragged(mouseDragged);
+        canvas.setOnKeyReleased(keyPressed);
+        partTypes.add("WATER");
+        partTypes.add("WALL");
+        partTypes.add("SAND");
+        partTypes.add("CONWAY");
 
         loop(gc);
     }
@@ -46,10 +62,54 @@ public class Main extends Application {
     EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>(){
 
         public void handle(MouseEvent mouseEvent){
+
+
             System.out.println((int)(Math.floor(mouseEvent.getX() / 10)));
-            parts.add(new particle((int)(Math.floor(mouseEvent.getX() / 10)), (int)(Math.floor(mouseEvent.getY() / 10)), "SAND", parts.size()));
+
         }
 
+
+
+    };
+
+    EventHandler<MouseEvent> mouseDragged = new EventHandler<MouseEvent>() {
+        public void handle(MouseEvent event){
+            mx = event.getX();
+            my = event.getY();
+            System.out.println("clicking");
+            spawning = true;
+        }
+
+    };
+
+    EventHandler<MouseEvent> mousePressed = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            if(event.isPrimaryButtonDown()) {
+                mx = event.getX();
+                my = event.getY();
+                System.out.println("clicking");
+                spawning = true;
+            } else if(event.isSecondaryButtonDown()){
+                sel++;
+                if(sel > partTypes.size() - 1) sel = 0;
+            }
+        }
+    };
+
+    EventHandler<KeyEvent> keyPressed = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            System.out.println("AYUP[");
+
+        }
+    };
+
+    EventHandler<MouseEvent> mouseReleased = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            spawning = false;
+        }
     };
 
     public void loop(GraphicsContext gc) {
@@ -58,6 +118,16 @@ public class Main extends Application {
             public void handle(long l) {
                 for(int i = 0; i < parts.size(); i++){
                     parts.get(i).update(parts);
+
+                }
+                int diam = 3;
+                if(spawning){
+
+                    parts.add(new particle(1 +(int)mx / 8,(int)my / 8, partTypes.get(sel), parts.size()));
+                    parts.add(new particle(-1 + (int)mx / 8,(int)my / 8, partTypes.get(sel), parts.size()));
+                    parts.add(new particle((int)mx / 8,1 + (int)my / 8, partTypes.get(sel), parts.size()));
+                    parts.add(new particle((int)mx / 8,-1 + (int)my / 8, partTypes.get(sel), parts.size()));
+
 
                 }
                 draw(gc);
@@ -72,7 +142,15 @@ public class Main extends Application {
 
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
-        gc.setFill(Color.CORNFLOWERBLUE);
+        int offset = 0;
+        gc.setFont(Font.font("Veranda", 30));
+        gc.setFill(Color.WHITE);
+        for (String type:partTypes) {
+            if(type.equals(partTypes.get(sel))) gc.setFill(Color.RED);
+            gc.fillText(type, offset, 40);
+            offset += 150;
+            gc.setFill(Color.WHITE);
+        }
         for (int i = 0; i < parts.size(); i++) {
             parts.get(i).draw(gc);
         }
--- particleSimulation/src/sample/particle.java
+++ particleSimulation/src/sample/particle.java
@@ -9,8 +9,11 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.paint.*;
+import java.util.concurrent.ThreadLocalRandom;
 import javafx.scene.canvas.*;
+import sun.security.ssl.Debug;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class particle {
@@ -18,7 +21,11 @@ public class particle {
     int y;
     String type;
     int id;
+    Color c;
+    boolean alive = false;
 
+    Boolean fixed = false;
+    int fixframe = 0;
     public int getX() {
         return x;
     }
@@ -31,78 +38,263 @@ public class particle {
         return id;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public particle(int x, int y, String type, int id) {
         this.x = x;
         this.y = y;
         this.type = type;
         this.id = id;
+        if(type.equals("SAND")) {
+            double rand = Math.random() * 60;
+            if (rand < 30) {
+                c = Color.LIGHTYELLOW;
+
+            }
+            if (rand > 30 && rand < 60) {
+                c = Color.SANDYBROWN;
+
+            }
+        }
+        if(type.equals("WATER")) {
+            double rand = Math.random() * 60;
+            if (rand < 30) {
+                c = Color.CORNFLOWERBLUE;
+
+            }
+            if (rand > 30 && rand < 60) {
+                c = Color.BLUE;
+
+            }
+        }
+        if(type.equals("WALL")){
+            c = Color.LIGHTGRAY;
+
+        }
     }
 
     public void update(ArrayList<particle> parts) {
-        if(type == "SAND") {
-                Boolean grounded = false;
-                Boolean falling = true;
-                int fall = 0;
-                if (this.y > 98) grounded = true;
-                if (grounded == false) {
-                    for (int i = 0; i < parts.size(); i++) {
-                        if (parts.get(i).getId() != this.id) {
-                            if (this.y + 1 == parts.get(i).getY() && this.x == parts.get(i).getX()) {
-                                grounded = true;
-                            }
+
+        switch(type) {
+            case "SAND": updateSand(parts);
+            break;
+            case "WATER": updateWater(parts);
+            break;
+            case "WALL": updateWall(parts);
+            break;
+            case "CONWAY": updateConway(parts);
+        }
+    }
+
+    private void updateConway(ArrayList<particle> parts){
+        double rand = Math.random() * 100;
+        if(rand < 10){
+            alive = true;
+        }
+        int neis = 0;
+        for (int i = 0; i < parts.size(); i++) {
+                 if (parts.get(i).getId() != this.id && parts.get(i).getType().equals("CONWAY")) {
+                     if((parts.get(i).getX() == this.x - 1 && parts.get(i).getY() == this.y) || (parts.get(i).getX() == this.x + 1 && parts.get(i).getY() == this.y) ||
+                             (parts.get(i).getX() == this.x && parts.get(i).getY() == this.y + 1) || (parts.get(i).getX() == this.x - 1 && parts.get(i).getY() == this.y - 1)){
+                         neis++;
+                     }
+                }
+            }
+
+        if(neis == 3 && alive == false){
+            alive = true;
+        }
+        if(alive == true && neis > 3){
+            alive = false;
+        }
+        if(alive == true && neis < 2){
+            alive = false;
+        }
+        }
+
+    private void updateWall(ArrayList<particle> parts){
+
+    }
+    private void updateWater(ArrayList<particle> parts){
+        Boolean grounded = false;
+        Boolean falling = false;
+        Boolean groundedWall = false;
+        int fall = 0;
+        if (this.y < 99) falling = true;
+
+        for (int i = 0; i < parts.size(); i++) {
+            if (parts.get(i).getId() != this.id) {
+                if (this.y + 1 == parts.get(i).getY() && this.x == parts.get(i).getX()) {
+                    if(parts.get(i).getType().equals("WALL")) groundedWall = true;
+                    falling = false;
+                }
+            }
+        }
+        if (falling == true) {
+            this.y += 1;
+            this.x += ThreadLocalRandom.current().nextInt(-2, 2 + 1);
+            falling = false;
+        } else {
+            this.x += ThreadLocalRandom.current().nextInt(-2, 2 + 1);
+
+            int dist = this.y;
+            int account = 0;
+            for (int i = 0; i < parts.size(); i++) {
+                if (parts.get(i).getX() == this.x && parts.get(i).getY() > this.y) account++;
+            }
+            if (dist + account == 99 || groundedWall) grounded = true;
+
+            Boolean CANLEFT = false;
+            Boolean CANRIGHT = false;
+            int fl = 0;
+            int fr = 0;
+            if (this.y < 99) {
+
+
+            }
+            int tl = 0;
+            int tr = 0;
+            for (int i = 0; i < parts.size(); i++) {
+                if (this.x - 1 == parts.get(i).getX()) {
+                    tl++;
+                }
+                if (this.x + 1 == parts.get(i).getX()) {
+                    tr++;
+                }
+            }
+            if (tl == 0 && tr == 0) {
+                int move = ThreadLocalRandom.current().nextInt(-2, 2 + 1);
+                if (this.x + 1 < 100 && this.x - 1 > 0) {
+                    this.x += move;
+                }
+                if(move < 0){
+                    if (tl == 0 && tr != 0) {
+                        if (this.x - 1 > 0) {
+                            this.x--;
+                        }
+
+                    } else {
+                        if (this.x + 1 < 100) {
+                            this.x++;
                         }
                     }
-                    if (grounded == false) {
-                        this.y += 1;
+                } else {
+                    if(tl == 0 && tr != 0){
+                        if (this.x + 1 < 100) {
+                            this.x++;
+                        }
+                    }
+                    else {
+                        if (this.x - 1 > 0) {
+                            this.x--;
+                        }
+
                     }
                 }
 
-            if(grounded == true) {
-                Boolean CANLEFT = false;
-                Boolean CANRIGHT = false;
-                int fl = 0;
-                int fr = 0;
-                if(this.y > 98) {
-                    for (int i = 0; i < parts.size(); i++) {
-                        if(parts.get(i).getId() != this.id) {
-                          if(this.x - 1 == parts.get(i).getX() && this.y + 1 == parts.get(i).getY()) {
+            }
+
+
+        }
+
+    }
+
+    private void updateSand(ArrayList<particle> parts){
+        int px = this.x;
+        int py = this.y;
+        if(!fixed) {
+
+            Boolean grounded = false;
+            Boolean falling = false;
+            Boolean groundedWall = false;
+            int fall = 0;
+            if (this.y < 99) falling = true;
+
+            for (int i = 0; i < parts.size(); i++) {
+                if (parts.get(i).getId() != this.id) {
+                    if (this.y + 1 == parts.get(i).getY() && this.x == parts.get(i).getX()) {
+                        if (parts.get(i).getType().equals("WALL")) groundedWall = true;
+                        falling = false;
+                    }
+                }
+            }
+            if (falling == true) {
+                this.y += 1;
+                falling = false;
+            }
+
+
+            int dist = this.y;
+            int account = 0;
+            for (int i = 0; i < parts.size(); i++) {
+                if (parts.get(i).getX() == this.x && parts.get(i).getY() > this.y) account++;
+            }
+            if (dist + account == 99 || groundedWall) grounded = true;
+
+            Boolean CANLEFT = false;
+            Boolean CANRIGHT = false;
+            int fl = 0;
+            int fr = 0;
+            if (this.y < 99) {
+                for (int i = 0; i < parts.size(); i++) {
+                    if (parts.get(i).getId() != this.id) {
+                        if (this.x - 1 == parts.get(i).getX() && this.y + 1 == parts.get(i).getY()) {
                             fl++;
-                            System.out.println("one left");
-                          }
-                          if(this.x + 1 == parts.get(i).getX() && this.y + 1 == parts.get(i).getY()){
-                             fr++;
-
-                          }
                         }
-                    }
+                        if (this.x + 1 == parts.get(i).getX() && this.y + 1 == parts.get(i).getY()) {
+                            fr++;
 
-                    if(fr == 0) {CANRIGHT = true;}
-                    if(fl == 0) {CANLEFT = true;}
 
-                    if(CANRIGHT && CANLEFT){
-                        double randr = Math.random() * 2;
-                        if(randr < 1){
-                            if(this.x > 0) {
-                                this.x -= 1;
-                            }
-
-                        } else {
-                            if(this.x < 99) {
-                                this.x += 1;
-                            }
                         }
-
                     }
                 }
+                if (fr == 0) CANRIGHT = true;
+                if (fl == 0) CANLEFT = true;
+                if (CANLEFT && CANRIGHT) {
+                    int rand = (int) Math.floor(Math.random() * 400);
+                    if (rand < 100 && rand > 50) {
+                        this.x--;
+                    } else if (rand < 50) {
+                        this.x++;
+                    }
+
+                } else if (CANLEFT && !CANRIGHT) {
+                    this.x--;
+
+                } else if (!CANLEFT && CANRIGHT) {
+                    this.x++;
+
+                }
+
             }
-
-
-
+            if (px == this.x && py == this.y) {
+                fixframe++;
+            } else{
+                fixframe = 0;
             }
+            if(fixframe > 25){
+                fixed = true;
+            }
+        }
         }
 
 
     public void draw(GraphicsContext gc){
-        gc.fillRect(this.x * 8, this.y * 8, 8, 8);
+        gc.setFill(c);
+        if(this.type != "CONWAY") {
+            gc.fillRect(this.x * 8, this.y * 8, 8, 8);
+        } else if (this.type == "CONWAY" && alive == true){
+            gc.fillRect(this.x * 8, this.y * 8, 8, 8);
+        }
     }
 }
